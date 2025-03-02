6c4220d6de4d6ef4a045218791317e399cb60c3b
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-03-02 18:09:33 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
   },
-  // Add this to help with CSS loading issues
-  optimizeFonts: false,
-  // Make sure any CSS modules get processed correctly
-  webpack(config) {
+  webpack: (config) => {
+    // Important: Return the modified config
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
