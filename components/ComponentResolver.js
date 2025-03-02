32d56936ee07fd241d844119d129bee9e3e9c694
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-03-02 18:35:20 +0400

    Update ComponentResolver.js

--- components/ComponentResolver.js
+++ components/ComponentResolver.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AITimeline from './AITimeline';
 import ModelComparison from './ModelComparison';
 import PromptEngineering from './PromptEngineering';
+import TransformerSVG from './TransformerSVG';
 
 // This component resolves and renders custom components from slide data
 const ComponentResolver = ({ componentName }) => {
@@ -12,6 +13,8 @@ const ComponentResolver = ({ componentName }) => {
       return <ModelComparison />;
     case 'PromptEngineering':
       return <PromptEngineering />;
+    case 'TransformerSVG':
+      return <TransformerSVG />;
     default:
       return <div className="text-red-500">Component "{componentName}" not found</div>;
   }
