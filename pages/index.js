db2b93c533271ccc43dc26dabea08db25f9d3466
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-03-02 18:14:17 +0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Presentation from '../components/Presentation';
+
+export default function Home() {
+  return (
+    <main>
+      <Presentation />
+    </main>
+  );
+}
