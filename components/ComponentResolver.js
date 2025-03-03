e15e46429710a696143b771627a6af57a164f6dd
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-03-03 08:50:32 +0400

    Update ComponentResolver.js

--- components/ComponentResolver.js
+++ components/ComponentResolver.js
@@ -3,10 +3,14 @@ import AITimeline from './AITimeline';
 import ModelComparison from './ModelComparison';
 import PromptEngineering from './PromptEngineering';
 import TransformerSVG from './TransformerSVG';
+import AIToolsGrid from './AIToolsGrid';
+import ContentToolsComparison from './ContentToolsComparison';
+import VisualContentShowcase from './VisualContentShowcase';
 
 // This component resolves and renders custom components from slide data
-const ComponentResolver = ({ componentName }) => {
+const ComponentResolver = ({ componentName, props = {} }) => {
   switch (componentName) {
+    // Module 1 components
     case 'AITimeline':
       return <AITimeline />;
     case 'ModelComparison':
@@ -15,6 +19,15 @@ const ComponentResolver = ({ componentName }) => {
       return <PromptEngineering />;
     case 'TransformerSVG':
       return <TransformerSVG />;
+      
+    // Module 2 components
+    case 'AIToolsGrid':
+      return <AIToolsGrid toolType={props.toolType || 'document'} />;
+    case 'ContentToolsComparison':
+      return <ContentToolsComparison />;
+    case 'VisualContentShowcase':
+      return <VisualContentShowcase />;
+      
     default:
       return <div className="text-red-500">Component "{componentName}" not found</div>;
   }
