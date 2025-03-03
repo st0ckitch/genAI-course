623fb074600e7c6717ab8e1cf415cba86ce1945a
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-03-03 09:52:29 +0400

    Update ComponentResolver.js

--- components/ComponentResolver.js
+++ components/ComponentResolver.js
@@ -7,6 +7,11 @@ import AIToolsGrid from './AIToolsGrid';
 import ContentToolsComparison from './ContentToolsComparison';
 import VisualContentShowcase from './VisualContentShowcase';
 
+// Import enhanced components
+import EnhancedAIToolsGrid from './EnhancedAIToolsGrid';
+import EnhancedContentToolsComparison from './EnhancedContentToolsComparison';
+import EnhancedVisualContentShowcase from './EnhancedVisualContentShowcase';
+
 // This component resolves and renders custom components from slide data
 const ComponentResolver = ({ componentName, props = {} }) => {
   switch (componentName) {
@@ -20,13 +25,21 @@ const ComponentResolver = ({ componentName, props = {} }) => {
     case 'TransformerSVG':
       return <TransformerSVG />;
       
-    // Module 2 components
+    // Module 2 components - original versions
     case 'AIToolsGrid':
       return <AIToolsGrid toolType={props.toolType || 'document'} />;
     case 'ContentToolsComparison':
       return <ContentToolsComparison />;
     case 'VisualContentShowcase':
       return <VisualContentShowcase />;
+      
+    // Enhanced Module 2 components
+    case 'EnhancedAIToolsGrid':
+      return <EnhancedAIToolsGrid toolType={props.toolType || 'document'} />;
+    case 'EnhancedContentToolsComparison':
+      return <EnhancedContentToolsComparison />;
+    case 'EnhancedVisualContentShowcase':
+      return <EnhancedVisualContentShowcase />;
       
     default:
       return <div className="text-red-500">Component "{componentName}" not found</div>;
